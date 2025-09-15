63e45c7da6f448a4d49b9526b57da66704cb0cd4
Diler Ciftci <diler.ciftci-94@hotmail.com>
2025-09-15 14:48:54 +0200

    Changed file location, imported Scanner class correctly and updated outputs.

--- src/Bank.java
+++ src/Bank.java
@@ -1,14 +1,16 @@
+import java.util.Scanner;
+
 public class Bank {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             System.out.println("Welcome to the bank terminal.");
-            System.out.println("Choose an action by entering a number from 1-4:")
-            System.out.println("1. Check balance.")
-            System.out.println("2. Deposit.")
-            System.out.println("3. Withdraw.")
-            System.out.println("4. Exit.")
+            System.out.println("Choose an action by entering a number from 1-4:");
+            System.out.println("1. Check balance.");
+            System.out.println("2. Deposit.");
+            System.out.println("3. Withdraw.");
+            System.out.println("4. Exit.");
         }
     }
 }
