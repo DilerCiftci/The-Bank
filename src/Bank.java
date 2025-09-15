5f53b6eb8b28f300acff448b2b2f0e07c93bc8aa
Diler Ciftci <diler.ciftci-94@hotmail.com>
2025-09-15 16:30:38 +0200

    Added comments

--- src/Bank.java
+++ src/Bank.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class Bank {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        // Imports scanner class
 
         int balance = 0;
+        // Creates "Balance" variable, balance starts at 0 by default
 
         while (true) {
             System.out.println("Welcome to the bank terminal.");
@@ -13,33 +15,38 @@ public class Bank {
             System.out.println("2. Deposit.");
             System.out.println("3. Withdraw.");
             System.out.println("4. Exit.");
+            // Loops outputs until "4" is chosen through the "choice" variable.
 
             int choice = scanner.nextInt();
+            // Variable used to choose different actions in the menu.
 
             if (choice == 1) {
                 System.out.println("Your total balance is: " + balance);
-            }
+            } // Simply chooses "Check balance" and outputs your current balance.
 
             else if (choice == 2) {
                 System.out.println("Enter amount to deposit: ");
                 int amount = scanner.nextInt();
                 balance += amount;
                 System.out.println("You deposited: " + amount);
-            }
+            } // Chooses "Deposit" action, takes amount as input from user, adds it to your
+              // balance, Outputs amount deposited.
 
             else if (choice == 3) {
                 System.out.println("Enter amount to withdraw: ");
                 int amount = scanner.nextInt();
                 balance -= amount;
                 System.out.println("You withdrew: " + amount);
-            }
+            } // Chooses "Withdraw" action, takes amount as input from user, removes it from
+              // your balance, Outputs amount Withdrawn.
 
             else if (choice == 4) {
                 System.out.println("Thank you for choosing our bank services.");
                 System.out.println("Goodbye!");
                 break;
-            }
+            } // Chooses "Exit" action, outputs farewell message & closes the loop with
+              // "break;"
         }
-        scanner.close();
+        scanner.close(); // Closes scanner
     }
 }
