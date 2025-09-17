85cbc28f3f0ef514a75bb54dd5f2e57a600aad72
Diler Ciftci <diler.ciftci-94@hotmail.com>
2025-09-17 17:07:29 +0200

    Added error handling

--- src/Bank.java
+++ src/Bank.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Bank {
     public static void main(String[] args) {
@@ -17,41 +18,53 @@ public class Bank {
             System.out.println("4. Exit.");
             // Loops outputs until "4" is chosen through the "choice" variable.
 
-            int choice = scanner.nextInt();
+            int choice;
             // Variable used to choose different actions in the menu.
 
-            if (choice == 1) {
-                System.out.println("Your total balance is: " + balance);
-            } // Simply chooses "Check balance" and outputs your current balance.
+            try {
+                choice = scanner.nextInt();
 
-            else if (choice == 2) {
-                System.out.println("Enter amount to deposit: ");
-                int amount = scanner.nextInt();
-                balance += amount;
-                System.out.println("You deposited: " + amount);
-            } /*
-               * Chooses "Deposit" action, takes amount as input from user, adds it to your
-               * balance, Outputs amount deposited.
-               */
+                if (choice == 1) {
+                    System.out.println("Your total balance is: " + balance);
+                } // Simply chooses "Check balance" and outputs your current balance.
 
-            else if (choice == 3) {
-                System.out.println("Enter amount to withdraw: ");
-                int amount = scanner.nextInt();
-                balance -= amount;
-                System.out.println("You withdrew: " + amount);
-            } /*
-               * Chooses "Withdraw" action, takes amount as input from user, removes it from
-               * your balance, Outputs amount Withdrawn.
-               */
+                else if (choice == 2) {
+                    System.out.println("Enter amount to deposit: ");
+                    int amount = scanner.nextInt();
+                    balance += amount;
+                    System.out.println("You deposited: " + amount);
+                } /*
+                   * Chooses "Deposit" action, takes amount as input from user, adds it to your
+                   * balance, Outputs amount deposited.
+                   */
 
-            else if (choice == 4) {
-                System.out.println("Thank you for choosing our bank services.");
-                System.out.println("Goodbye!");
-                break;
-            } /*
-               * Chooses "Exit" action, outputs farewell message & closes the loop with
-               * "break;"
-               */
+                else if (choice == 3) {
+                    System.out.println("Enter amount to withdraw: ");
+                    int amount = scanner.nextInt();
+                    balance -= amount;
+                    System.out.println("You withdrew: " + amount);
+                } /*
+                   * Chooses "Withdraw" action, takes amount as input from user, removes it from
+                   * your balance, Outputs amount Withdrawn.
+                   */
+
+                else if (choice == 4) {
+                    System.out.println("Thank you for choosing our bank services.");
+                    System.out.println("Goodbye!");
+                    break;
+                } /*
+                   * Chooses "Exit" action, outputs farewell message & closes the loop with
+                   * "break;"
+                   */
+                else {
+                    System.out.println("Invalid choice. Please select a number between 1 and 4. ");
+                }
+            }
+
+            catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter numbers only.");
+                scanner.nextLine();
+            }
         }
         scanner.close(); // Closes scanner
     }
